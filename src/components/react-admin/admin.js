873325f940c9c044dc6a10bebb3e3c1f94a12671
Miguel <733350@alu.murciaeduca.es>
2023-02-01 09:43:47 +0100

    Autentificacion con tokens

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -1,14 +1,15 @@
 import { Admin, Resource } from 'react-admin';
+import { useState } from 'react';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from 'components/react-admin/users';
 import { CustomerList, CustomerEdit, CustomerCreate } from 'components/react-admin/customers';
 import { ArtworkList} from 'components/react-admin/artworks';
-
+import { default as AuthProvider } from 'components/react-admin/authProvider';
+import { default as Login } from 'pages/login'
 
 //TODO eliminar las dos líneas siguientes
 import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
 import { MigrationList, MigrationEdit, MigrationCreate } from 'components/react-admin/migrations';
-
 
 import jsonapiClient from 'ra-jsonapi-client';
 
@@ -28,22 +29,34 @@ import { AdminLayout } from 'components/react-admin/adminLayout';
 //Este para Laravel Controllers
 const dataProvider = jsonapiClient('http://encuentro.test/api');
 
-const RAdmin = () => (
+const RAdmin = () => {
+  function handleDataProvider(dataProvider) {
+    setDataProvider(() => dataProvider)
+  }
+  const myLogin = <Login handleDataProvider={handleDataProvider} />
+  const API_URL = `${process.env.REACT_APP_BACKEND_URL}/api`
+  const [dataProvider, setDataProvider] = useState(null)
+
+  if (!dataProvider) {
+    handleDataProvider(jsonapiClient(API_URL))
+  }
+  return(
   <Admin
     basename="/dashboard"
     dataProvider={dataProvider}
-    layout={AdminLayout}
+    authProvider={AuthProvider}
+    loginPage={myLogin}
   >
     <Resource name="customers" list={CustomerList} icon={CustomerIcon} edit={CustomerEdit} create={CustomerCreate} />
 
     <Resource name="artworks" list={ArtworkList} icon={ArtworkIcon} />
-
 
     <Resource name="migrations"
       list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate}/>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
   </Admin>
-)
+  
+)}
 
 export default RAdmin;
