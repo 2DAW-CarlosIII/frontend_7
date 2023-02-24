8d9242e29c95d21123cfe6e644292ffffb41b440
Miguel <733350@alu.murciaeduca.es>
2023-02-24 22:00:29 +0100

    correccion landing

--- src/components/Landing-Page/Landing.js
+++ src/components/Landing-Page/Landing.js
@@ -1,17 +1,15 @@
-import Body from "./Body/Body";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
+import Body from './Body/Body';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
 
-const Landing = () => {
-
-    return(
+const Landing = () => (
         
     <main>
         <Header/>
         <Body/>
         <Footer/>
     </main>
-    )
-}
+    
+)
 
 export default Landing;
